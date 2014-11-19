c6a9e25f6d3bc87981e5803a66e2b6067b618acb
Mark Barrasso <mark.barrasso@live.com>
2014-11-19 11:16:51 -0500

    updated to portrait mode, fixed some images, added level select class

---
+++ Source/LevelSelect.h
@@ -0,0 +1,13 @@
+//
+//  LevelSelect.h
+//  Blocka
+//
+//  Created by Mark on 11/19/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface LevelSelect : CCNode
+
+@end
---
+++ Source/LevelSelect.m
@@ -0,0 +1,13 @@
+//
+//  LevelSelect.m
+//  Blocka
+//
+//  Created by Mark on 11/19/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "LevelSelect.h"
+
+@implementation LevelSelect
+
+@end
