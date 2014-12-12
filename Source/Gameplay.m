734e8c21d5e4293769f852c314aacb70ce774094
Mark Barrasso <mark.barrasso@live.com>
2014-12-11 20:50:00 -0500

    added level to gameplay scene

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -9,5 +9,33 @@
 #import "Gameplay.h"
 
 @implementation Gameplay
+{
+    // physics
+    CCPhysicsNode *_physicsNode;
+    
+    // levels
+    CCNode *_levelNode;
+}
+
+- (void)onEnter
+{
+    [super onEnter];
+    
+    // Create string in order to load current/next levels
+    NSString *levelString = [NSString stringWithFormat:@"Levels/Level1"];
+    
+    // tell this scene to accept touches
+    self.userInteractionEnabled = TRUE;
+    
+    //Load the level
+    CCScene *level = [CCBReader loadAsScene:levelString];
+    [_levelNode addChild: level];
+}
+
+- (void)onExit
+{
+    // deallocate
+    [super onExit];
+}
 
 @end
